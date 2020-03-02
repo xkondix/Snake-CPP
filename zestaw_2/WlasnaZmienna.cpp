bbcdd5c957a1450cb96f520dd171b1451c54b777
xkondix <konrad.kowalczyk.98@gmail.com>
2020-03-02 10:17:01 +0100

    static

--- zestaw_2/WlasnaZmienna.cpp
+++ zestaw_2/WlasnaZmienna.cpp
@@ -2,6 +2,9 @@
 #include "Zmienna.h"
 using namespace std;
 
+static int licznik = 0;
+
+myFloat value(myFloat f, float f1);
 
 int main()
 {
@@ -10,8 +13,21 @@ myFloat f;
 f.data = 2.5;
 
 cout<<f.data<<endl;
-cout<< getInt(f);
+cout<< getInt(f)<<endl;
 
+value(f,5.2);
+
+cout<<f.data<<endl;
+
+cout<<"licznik = "<<licznik;
 
 return 0;
+}
+
+myFloat value(myFloat f, float f1)
+{
+
+    licznik++;
+    f.data = f1;
+    return f;
 }
