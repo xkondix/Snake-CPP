b74b74ed8cace129efb242dfaf4f403dd5598b03
xkondix <konrad.kowalczyk.98@gmail.com>
2020-03-16 09:17:51 +0100

    funkcje

---
+++ 2020.03.16/funkcje.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void funckja();
+int i(int a);
+
+int main(){
+
+    funckja();
+    cout<<endl<<i(5);
+    return 0;
+}
+
+void funckja()
+{
+  cout<<"Ja tego prawie zawsze używałem w liceum";
+}
+int i(int a)
+{
+return a;
+}
+
