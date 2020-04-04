4fafdfb2ef38d005349922becddc470e708f6b52
xkondix <konrad.kowalczyk.98@gmail.com>
2020-04-04 19:17:05 +0200

    zadania

---
+++ zadania/ZadAdresowaniePamieci.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a; //deklarowanie zmiennej -> alokowanie gdzieś miejsca w pamięci o wartości int (u mnie 4 bajty)
+cout<<"adres zmiennej a = "<<&a<<endl;
+cout<<"wartość zmiennej a, jak nie jest zainicjowana = "<<a<<endl; //losowa z przedziału int, która tam się znajduje
+
+a = 5; //inicjowanie zmiennej -> podstawienie pod te miejsce w pamięci wartości 5 - > dalej zajmuje 32 bity
+cout<<"wartość zmiennej a = "<<a<<endl;
+
+
+
+
+return 0;
+}
---
+++ zadania/ZadIq.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int tab[] = {11,22,33,44,55}, i = 3, *q;
+	q = tab;
+	cout << "q[i]   = " << q[i] << endl;
+	cout << "i[q]   = " << i[q] << endl;   //<-- Proszę to wyjaśnić
+
+	//wyjasnienie
+	cout<<"-------------------------------"<<endl;
+
+	cout<<"numery pamieci i oraz q "<< &i<<" "<<q<<endl;
+	cout<<"wartosci i oraz q "<<i<<" "<<*q<<endl;
+    cout<<"q+1 = "<<q+1<<endl;
+	cout<<"q+2 = "<<q+2<<endl;
+	cout<<"q+3 = "<<q+3<<endl;
+    cout<<"adres i+q = "<<q+i<<endl;
+    cout<<"*q = "<<*(q+3)<<endl;
+    cout<<"i[tab] = "<<i[tab]<<endl;
+
+
+
+	cout<<"rozwiazanie *(i+q) = "<<*(i+q); // i = 3, a q = jakis tam adres -> jest to równe zapisowi q[i] lub i[q]
+	//Jest to możliwe dzięki temu ze q to wskaznik, wiec nie wazne co tam damy bedzie to zapis jak powyżej.
+	//Oczywiście i[tab] też zadziała, bo tablica to wskaznik na 1 element
+
+    return 0;
+}
+
